0de0e5d652c955d4f952e877feb5472fb8296f5f
begin0dev <prosonic1@gmail.com>
2022-04-23 17:38:27 +0900

    FIX: calendar

--- src/components/register/CalendarModal.tsx
+++ src/components/register/CalendarModal.tsx
@@ -1,8 +1,9 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import Modal from "react-native-modal";
 
 import { themeColors } from "../../styles/colors";
 import { Calendar } from "../common";
+import { IcClose } from "../../assets/svgs";
 
 interface Props {
   defaultDate?: Date | null | undefined;
@@ -13,9 +14,13 @@ interface Props {
 
 function CalendarModal({ isVisible, defaultDate, onPressDate, onClose }: Props) {
   return (
-    <Modal isVisible={isVisible} onBackButtonPress={onClose}>
-      <Text>aaaa</Text>
+    <Modal isVisible={isVisible} onBackButtonPress={onClose} backdropOpacity={0.4}>
       <View style={styles.wrapper}>
+        <View style={styles.closeBtnWrapper}>
+          <TouchableOpacity style={styles.closeBtn} onPress={onClose}>
+            <IcClose color={themeColors.SECONDARY_BACKGROUND} />
+          </TouchableOpacity>
+        </View>
         <View style={styles.content}>
           <Calendar defaultDate={defaultDate} onPressDate={onPressDate} />
         </View>
@@ -27,9 +32,26 @@ function CalendarModal({ isVisible, defaultDate, onPressDate, onClose }: Props) 
 export default CalendarModal;
 
 const styles = StyleSheet.create({
-  closeBtn: {},
-  wrapper: {
+  closeBtnWrapper: {
+    zIndex: 100,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
     alignItems: "center",
+  },
+  closeBtn: {
+    width: 32,
+    height: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: themeColors.TEXT_0,
+    borderRadius: 16,
+  },
+  wrapper: {
+    position: "relative",
+    alignItems: "center",
+    paddingVertical: 16,
   },
   content: {
     width: 332,
